64ff919754272566e673132c66a3d39eea571bc0
AndyLithia <andy19981206@gmail.com>
2017-07-20 23:47:37 +0800

    Initial Commit

---
+++ Strategy/StrategySynchronisedSwimming/StrategySynchronisedSwimming.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using xna = Microsoft.Xna.Framework;
+using URWPGSim2D.Common;
+using URWPGSim2D.StrategyLoader;
+using URWPGSim2D.StrategyHelper;
+using System.IO;
+
+namespace URWPGSim2D.Strategy {
+    public class Strategy : MarshalByRefObject, IStrategy {
+        #region reserved code never be changed or removed
+        /// <summary>
+        /// override the InitializeLifetimeService to return null instead of a valid ILease implementation
+        /// to ensure this type of remote object never dies
+        /// </summary>
+        /// <returns>null</returns>
+        public override object InitializeLifetimeService() {
+            //return base.InitializeLifetimeService();
+            return null; // makes the object live indefinitely
+        }
+        #endregion
+
+        /// <summary>
+        /// 决策类当前对象对应的仿真使命参与队伍的决策数组引用 第一次调用GetDecision时分配空间
+        /// </summary>
+        private Decision[] decisions = null;
+        /// <summary>
+        /// 获取队伍名称 在此处设置参赛队伍的名称
+        /// </summary>
+        /// <returns>队伍名称字符串</returns>
+        public string GetTeamName() {
+            return "SYSU East Repairers";
+        }
+
+        public static int isDirectionRight(float a, float b) {
+            if (a > Math.PI)
+                a -= (float)(2 * Math.PI);
+            if (a < -Math.PI)
+                a += (float)(2 * Math.PI);
+
+            if (b > Math.PI)
+                b -= (float)(2 * Math.PI);
+            if (b < -Math.PI)
+                b += (float)(2 * Math.PI);
+
+            if (a - b > 0.15)
+                return 1;//a在b右边
+            else if (a - b < -0.15)
+                return -1; //a在b左边
+            else
+                return 0;
+        }
+
+        public static bool allEqual(int[] group, int value, int start, int end) {
+            for (int i = start; i <= end; i++)
+                if (group[i] != value)
+                    return false;
+            return true;
+        }
+
+        // 判断group数组内从start到end间是否有不少于numToPass数量的项等于value
+        public static bool isNearlyStable(int[] group, int value, int start, int end, int numToPass) {
+            int counter = 0;
+            for (int i = start; i <= end; i++)
+                if(group[i] == value)
+                    counter++;
+            return (counter >= numToPass) ? true : false;
+        }
+
+        // 
+        public static bool allFishNearPoints(RoboFish[] group, xna.Vector3[] target, int start, int end, float aRadius) {
+            for(int i=start; i<=end; i++)
+                if ((Math.Pow(group[i].PositionMm.X-target[i].X,2)+ Math.Pow(group[i].PositionMm.Z - target[i].Z, 2)) > Math.Pow(aRadius,2))
+                        return false;
+            return true;
+        }
+
+        public static void stopFish(ref Decision decision, int i) {
+            decision.VCode = 0;
+            decision.TCode = 7;
+            timeForPoseToPose[i] = 0;
+        }
+
+        // 获得两个坐标间的距离
+        public static float getVectorDistance(xna.Vector3 a, xna.Vector3 b) {
+            return (float)Math.Sqrt((Math.Pow((a.X - b.X), 2d) + Math.Pow((a.Z - b.Z), 2d)));
+        }
+
+        public static void fishToPoint(ref Decision decisions, RoboFish fish, xna.Vector3 targetePoint, float targetDirection, int noOfFish, ref int[] timeForPoseToPose, int[] flag) {
+            switch (flag[noOfFish]) {
+                case 0:
+                    if (getVectorDistance(targetePoint, fish.PositionMm) > 100) {
+                        Helpers.PoseToPose(ref decisions, fish, targetePoint, targetDirection, 6f, 10f, 100, ref timeForPoseToPose[noOfFish]);
+                    }
+                    //  Helpers.PoseToPose(ref decisions, fish, targetePoint, targetDirection, 6f, 10f, 50, ref timeForPoseToPose[noOfFish]);
+                    if (getVectorDistance(targetePoint, fish.PositionMm) < 100) {
+                        flag[noOfFish] = 1;
+                    }
+                    break;
+                case 1:
+                    if (isDirectionRight(targetDirection, fish.BodyDirectionRad) < 0) {
+                        decisions.TCode = 0;
+                        decisions.VCode = 1;
+                    } else if (isDirectionRight(targetDirection, fish.BodyDirectionRad) > 0) {
+                        decisions.TCode = 14;
+                        decisions.VCode = 1;
+                    } else {
+                        flag[noOfFish] = 2;
+                        stopFish(ref decisions, noOfFish);
+                    }
+                    if (getVectorDistance(targetePoint, fish.PositionMm) > 150)
+                        flag[noOfFish] = 0;
+                    break;
+                case 2:
+                    if (isDirectionRight(targetDirection, fish.BodyDirectionRad) < 0) {
+                        decisions.TCode = 0;
+                        decisions.VCode = 1;
+                    } else if (isDirectionRight(targetDirection, fish.BodyDirectionRad) > 0) {
+                        decisions.TCode = 14;
+                        decisions.VCode = 1;
+                    } else {
+                        stopFish(ref decisions, noOfFish);
+                    }
+                    if (getVectorDistance(targetePoint, fish.PositionMm) > 150)
+                        flag[noOfFish] = 0;
+                    break;
+                default:
+                    decisions.TCode = 7;
+                    decisions.VCode = 0;
+                    break;
+            }
+        }
+
+        public static void dribbleFishToPoint(ref Decision decisions, RoboFish fish, xna.Vector3 targetePoint, float targetDirection, int noOfFish, int[] flag) {
+            switch (flag[noOfFish]) {
+                case 0:
+                    if (getVectorDistance(targetePoint, fish.PositionMm) > 100)
+                        Helpers.Dribble(ref decisions, fish, targetePoint, targetDirection, 5f, 10f, 50f, 14, 10, 6, 100, false);
+                    if (getVectorDistance(targetePoint, fish.PositionMm) < 100)
+                        flag[noOfFish] = 1;
+                    break;
+                case 1:
+                    if (isDirectionRight(targetDirection, fish.BodyDirectionRad) < 0) {
+                        decisions.TCode = 0;
+                        decisions.VCode = 1;
+                    } else if (isDirectionRight(targetDirection, fish.BodyDirectionRad) > 0) {
+                        decisions.TCode = 14;
+                        decisions.VCode = 1;
+                    } else {
+                        flag[noOfFish] = 2;
+                        stopFish(ref decisions, noOfFish);
+                    }
+                    if (getVectorDistance(targetePoint, fish.PositionMm) > 150)
+                        flag[noOfFish] = 0;
+                    break;
+                case 2:
+                    if (isDirectionRight(targetDirection, fish.BodyDirectionRad) < 0) {
+                        decisions.TCode = 0;
+                        decisions.VCode = 1;
+                    } else if (isDirectionRight(targetDirection, fish.BodyDirectionRad) > 0) {
+                        decisions.TCode = 14;
+                        decisions.VCode = 1;
+                    } else
+                        stopFish(ref decisions, noOfFish);
+                    if (getVectorDistance(targetePoint, fish.PositionMm) > 150)
+                        flag[noOfFish] = 0;
+                    break;
+                default:
+                    decisions.TCode = 7;
+                    decisions.VCode = 0;
+                    break;
+
+            }
+        }
+
+        public static int completeCircle = 0;
+        Decision[] preDecisions = null;
+
+        private static int flag = 0;//主函数标志值
+
+        private static int timeflag = 0;
+        private static int[] timeForPoseToPose = new int[11];
+        private static bool complete = false;
+        private static int[] startRoadflag = new int[11];
+        private static int[] hillflag = new int[11];
+        private static int[] oneflag = new int[11];
+        private static int[] playflag = new int[11];
+        private static int[] circleflag = new int[11];
+
+        /// <summary>
+        /// 获取当前仿真使命（比赛项目）当前队伍所有仿真机器鱼的决策数据构成的数组
+        /// </summary>
+        /// <param name="mission">服务端当前运行着的仿真使命Mission对象</param>
+        /// <param name="teamId">当前队伍在服务端运行着的仿真使命中所处的编号 
+        /// 用于作为索引访问Mission对象的TeamsRef队伍列表中代表当前队伍的元素</param>
+        /// <returns>当前队伍所有仿真机器鱼的决策数据构成的Decision数组对象</returns>
+
+        #region 动态心
+        // 心造型，顺时针旋转，假设开始时所有的鱼接近初始目标点（需要重编号）
+        // 模块负责人：联航
+        // v0.1 07.19: 初始版本，确定心脏的目标点
+        private static int state = 0;
+
+        class movingHeartClass {
+
+            
+            private static int round = 0;
+
+            // 稳定标记
+            private static int[] eqFlag = new int[11];
+
+            // 阶段状态标记
+            private static bool heartStageFlag = true;
+
+            static xna.Vector3[] targetVector=new xna.Vector3[9];
+            static float[] targetAngle = new float[9];
+
+            static int[] targetVectorX1 = { -1100, -528, 120, 738, 1200, 918, 300, -450, -1100 };
+            static int[] targetVectorZ1 = { -510, -800, -320, -800, -288, 320, 1000, 800, 100 };
+            static float[] targetAngle1 = { 300, 0, 0, 0, 60, 130, 130, 230, 230 };
+
+            static int[] targetVectorX2 = { -1026, -798, -168, 372, 1002, 1032, 642, -60, -648 };
+            static int[] targetVectorZ2 = { -96, -708, -456, -630, -558, 108, 762, 1068, 594 };
+            static float[] targetAngle2 = { 260, 320, 60, 300, 40, 100, 130, 180, 230 };
+
+            private static int fishIDShift = 0;
+
+            private static float deg2rad(float deg) {
+                if (deg > 180) deg -= 360;
+                return (float)(Math.PI * deg / 180);
+            }
+
+            public static void movingHeart(ref Mission mission, int teamId, ref Decision[] decisions) {
+                // StreamWriter log = new StreamWriter("C:\\log.txt", true);
+                // log.Close();
+
+                // 仿真周期毫秒数
+                int msPerCycle = mission.CommonPara.MsPerCycle;
+                RoboFish[] fish = {
+                    mission.TeamsRef[teamId].Fishes[1],
+                    mission.TeamsRef[teamId].Fishes[2],
+                    mission.TeamsRef[teamId].Fishes[3],
+                    mission.TeamsRef[teamId].Fishes[4],
+                    mission.TeamsRef[teamId].Fishes[5],
+                    mission.TeamsRef[teamId].Fishes[6],
+                    mission.TeamsRef[teamId].Fishes[7],
+                    mission.TeamsRef[teamId].Fishes[8],
+                    mission.TeamsRef[teamId].Fishes[9] };
+                // Reserved，需要重编号功能以尽可能减少时间
+                if (state == 0) {
+                    // 初始化
+                    /*
+                    for(int i = 0; i < 9; i++) 
+                        fish[i] = mission.TeamsRef[teamId].Fishes[i + 1];
+                    */
+                    state = 1;
+                    for (int i = 0; i < 11; i++) {
+                        eqFlag[i] = 0;
+                        timeForPoseToPose[i] = 0;
+                    }
+                    
+                    return;
+                } else if (state == 1) {
+                    // 装载心的第一个状态
+                    if (heartStageFlag) {
+                        for (int i = 0; i < 9; i++) {
+                            targetVector[i].X = targetVectorX1[i];
+                            targetVector[i].Z = targetVectorZ1[i];
+                            targetAngle[i] = deg2rad(targetAngle1[i]);
+                            targetVector[i].Y = 0;
+                        }
+                    } else {
+                        for (int i = 0; i < 9; i++) {
+                            targetVector[i].X = targetVectorX2[i];
+                            targetVector[i].Z = targetVectorZ2[i];
+                            targetAngle[i] = deg2rad(targetAngle2[i]);
+                            targetVector[i].Y = 0;
+                        }
+                    }
+                    
+                    state = 2;
+                    return;
+                } else if (state == 2) {
+                    // 行动至心的第一个状态
+                    for (int i = 0; i < 9; i++) {
+                        int j;
+                        if (i - fishIDShift <0) j = 9 + i - fishIDShift;
+                        else j = i - fishIDShift;
+                        fishToPoint(ref decisions[j + 1], fish[j], targetVector[i], targetAngle[i], j + 2, ref timeForPoseToPose, eqFlag);
+                    }
+
+                    if (allEqual(eqFlag, 2, 3, 10)) {
+                        for (int i = 0; i < 11; i++) {
+                            eqFlag[i] = 0;
+                            timeForPoseToPose[i] = 0;
+                        }
+                        // state = 1;
+                        state = 3;
+                    }
+                    return;
+                } else if (state == 3) {
+                    //fishSwap(ref fish);
+                    if(heartStageFlag) fishIDShift++;
+                    heartStageFlag = !heartStageFlag;
+                    state = 1;
+
+                    return;
+                }
+                return;
+
+            }
+
+        }
+        #endregion
+
+        public Decision[] GetDecision(Mission mission, int teamId) {
+            // 决策类当前对象第一次调用GetDecision时Decision数组引用为null
+            if (decisions == null) {// 根据决策类当前对象对应的仿真使命参与队伍仿真机器鱼的数量分配决策数组空间
+                decisions = new Decision[mission.CommonPara.FishCntPerTeam];
+                preDecisions = new Decision[mission.CommonPara.FishCntPerTeam];
+            }
+            mission.CommonPara.MsPerCycle = 100;
+            #region 决策计算过程 需要各参赛队伍实现的部分
+            #region 策略编写帮助信息
+            //====================我是华丽的分割线====================//
+            //======================策略编写指南======================//
+            //1.策略编写工作直接目标是给当前队伍决策数组decisions各元素填充决策值
+            //2.决策数据类型包括两个int成员，VCode为速度档位值，TCode为转弯档位值
+            //3.VCode取值范围0-14共15个整数值，每个整数对应一个速度值，速度值整体但非严格递增
+            //有个别档位值对应的速度值低于比它小的档位值对应的速度值，速度值数据来源于实验
+            //4.TCode取值范围0-14共15个整数值，每个整数对应一个角速度值
+            //整数7对应直游，角速度值为0，整数6-0，8-14分别对应左转和右转，偏离7越远，角度速度值越大
+            //5.任意两个速度/转弯档位之间切换，都需要若干个仿真周期，才能达到稳态速度/角速度值
+            //目前运动学计算过程决定稳态速度/角速度值接近但小于目标档位对应的速度/角速度值
+            //6.决策类Strategy的实例在加载完毕后一直存在于内存中，可以自定义私有成员变量保存必要信息
+            //但需要注意的是，保存的信息在中途更换策略时将会丢失
+            //====================我是华丽的分割线====================//
+            //=======策略中可以使用的比赛环境信息和过程信息说明=======//
+            //场地坐标系: 以毫米为单位，矩形场地中心为原点，向右为正X，向下为正Z
+            //            负X轴顺时针转回负X轴角度范围为(-PI,PI)的坐标系，也称为世界坐标系
+            //mission.CommonPara: 当前仿真使命公共参数
+            //mission.CommonPara.FishCntPerTeam: 每支队伍仿真机器鱼数量
+            //mission.CommonPara.MsPerCycle: 仿真周期毫秒数
+            //mission.CommonPara.RemainingCycles: 当前剩余仿真周期数
+            //mission.CommonPara.TeamCount: 当前仿真使命参与队伍数量
+            //mission.CommonPara.TotalSeconds: 当前仿真使命运行时间秒数
+            //mission.EnvRef.Balls: 
+            //当前仿真使命涉及到的仿真水球列表，列表元素的成员意义参见URWPGSim2D.Common.Ball类定义中的注释
+            //mission.EnvRef.FieldInfo: 
+            //当前仿真使命涉及到的仿真场地，各成员意义参见URWPGSim2D.Common.Field类定义中的注释
+            //mission.EnvRef.ObstaclesRect: 
+            //当前仿真使命涉及到的方形障碍物列表，列表元素的成员意义参见URWPGSim2D.Common.RectangularObstacle类定义中的注释
+            //mission.EnvRef.ObstaclesRound:
+            //当前仿真使命涉及到的圆形障碍物列表，列表元素的成员意义参见URWPGSim2D.Common.RoundedObstacle类定义中的注释
+            //mission.TeamsRef[teamId]:
+            //决策类当前对象对应的仿真使命参与队伍（当前队伍）
+            //mission.TeamsRef[teamId].Para:
+            //当前队伍公共参数，各成员意义参见URWPGSim2D.Common.TeamCommonPara类定义中的注释
+            //mission.TeamsRef[teamId].Fishes:
+            //当前队伍仿真机器鱼列表，列表元素的成员意义参见URWPGSim2D.Common.RoboFish类定义中的注释
+            //mission.TeamsRef[teamId].Fishes[i].PositionMm和PolygonVertices[0],BodyDirectionRad,VelocityMmPs,
+            //                                   AngularVelocityRadPs,Tactic:
+            //当前队伍第i条仿真机器鱼鱼体矩形中心和鱼头顶点在场地坐标系中的位置（用到X坐标和Z坐标），鱼体方向，速度值，
+            //                                   角速度值，决策值
+            //====================我是华丽的分割线====================//
+            //========================典型循环========================//
+            //for (int i = 0; i < mission.CommonPara.FishCntPerTeam; i++)
+            //{
+            //  decisions[i].VCode = 0; // 静止
+            //  decisions[i].TCode = 7; // 直游
+            //}
+            //====================我是华丽的分割线====================//
+            #endregion
+
+            #endregion
+            movingHeartClass.movingHeart(ref mission, teamId, ref decisions);
+            // movingCircle(ref mission, teamId, ref decisions);
+            return decisions;
+        }
+    }
+}
+
+
+
+
