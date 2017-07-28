d156d81e49873b879f265614dcab28538fc4cb74
chenyjade <www.877975661@qq.com>
2017-07-29 02:12:19 +0800

    v0.5

--- Strategy/StrategyBallMoving/StrategyBallMoving.cs
+++ Strategy/StrategyBallMoving/StrategyBallMoving.cs
@@ -229,7 +229,7 @@ namespace URWPGSim2D.Strategy
                     }
                     break;
                 case 6: //移动到球1的后方
-                    temp = new xna.Vector3(ball[1].X + (float)1.5 * ballR, 0, ball[1].Z + (float)1.5 * ballR);
+                    temp = new xna.Vector3(ball[1].X + (float)2.5 * ballR, 0, ball[1].Z - (float)2.5 * ballR);
                     Helpers.PoseToPose(ref decisions[1], rFish2, temp, deg2rad(0), 30, 100, CycleTime, ref times);
                     if (getDistance(temp, my_fish2.body) < 100)
                     {
@@ -390,7 +390,7 @@ namespace URWPGSim2D.Strategy
                     }
                     break;
                 case 6: //移动到球2的后方
-                    temp = new xna.Vector3(ball[2].X + (float)1.5 * ballR, 0, ball[2].Z + (float)1.5 * ballR);
+                    temp = new xna.Vector3(ball[2].X + (float)2.5 * ballR, 0, ball[2].Z + (float)2.5 * ballR);
                     Helpers.PoseToPose(ref decisions[0], rFish1, temp, deg2rad(0), 30, 100, CycleTime, ref times);
                     if (getDistance(temp, my_fish1.body) < 100)
                     {
@@ -455,4 +455,3 @@ namespace URWPGSim2D.Strategy
         }
     }
 }
-
